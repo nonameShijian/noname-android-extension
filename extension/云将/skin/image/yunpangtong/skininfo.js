70feab4c358a56327f2c5057bcdaffc878c2ce5f
shijian <2954700422@qq.com>
2022-07-01 02:07:51 +0800

    增加云将扩展

---
+++ extension/云将/skin/image/yunpangtong/skininfo.js
@@ -0,0 +1,103 @@
+'use strict';
+window.qhly_import_safe(function(lib,game,ui,get,ai,_status){
+	var obj = {
+    "name": "yunpangtong",
+    "origin": {
+        "skill": {
+                "yunxiance": {
+                    "content": "才华得现，放手一搏。<br>赤壁得胜，八九乃我之功。"
+                },
+                "yunfengming": {
+                    "content": "凤雏岂能消亡？<br>心怀天下者，必当有破釜沉舟的勇气。"
+                },
+                "yunniepan": {
+                    "content": "天命？哈哈哈～<br>天命难违？哈哈哈哈～"
+                },
+                "die": {
+                    "content": "看来……命中当有此劫……"
+                }
+            }
+    },
+    "skin": {
+        "yunpangtong2": {
+            "level": "$extension/云将/biaoqian/ico_jingpin.png",
+            "translation": "智计绝伦",
+            "order": 2,
+            "levelStyle": {
+                "width": "45px",
+                "height": "45px",
+                "bottom": "calc(0.00%)",
+                "left": "calc(65.00%)",
+                "opacity": "1"
+            },
+            "skill": {
+                "yunxiance": {
+                    "content": "才华得现，放手一搏。<br>赤壁得胜，八九乃我之功。"
+                },
+                "yunfengming": {
+                    "content": "凤雏岂能消亡？<br>心怀天下者，必当有破釜沉舟的勇气。"
+                },
+                "yunniepan": {
+                    "content": "天命？哈哈哈～<br>天命难违？哈哈哈哈～"
+                },
+                "die": {
+                    "content": "看来……命中当有此劫……"
+                }
+            }
+        },
+        "yunpangtong5": {
+            "level": "$extension/云将/biaoqian/ico_chuanshuo.png",
+            "translation": "凤凰于飞",
+            "order": 5,
+            "levelStyle": {
+                "width": "45px",
+                "height": "45px",
+                "bottom": "calc(0.00%)",
+                "left": "calc(65.00%)",
+                "opacity": "1"
+            },
+            "skill": {
+                "yunxiance": {
+                    "content": "才华得现，放手一搏。<br>赤壁得胜，八九乃我之功。"
+                },
+                "yunfengming": {
+                    "content": "凤雏岂能消亡？<br>心怀天下者，必当有破釜沉舟的勇气。"
+                },
+                "yunniepan": {
+                    "content": "天命？哈哈哈～<br>天命难违？哈哈哈哈～"
+                },
+                "die": {
+                    "content": "看来……命中当有此劫……"
+                }
+            }
+        },
+        "yunpangtong7": {
+            "level": "$extension/云将/biaoqian/ico_diancang.png",
+            "translation": "小小凤雏",
+            "order": 7,
+            "levelStyle": {
+                "width": "55px",
+                "height": "25px",
+                "bottom": "calc(4.00%)",
+                "left": "calc(57.00%)",
+                "opacity": "1"
+            },
+            "skill": {
+                "yunxiance": {
+                    "content": "先声夺人！<br>成事在天，谋事在人。"
+                },
+                "yunfengming": {
+                    "content": "置之死地方能后生！"
+                },
+                "yunniepan": {
+                    "content": "燃烧吧！"
+                },
+                "die": {
+                    "content": "对天起誓，以明我志。"
+                }
+            }
+        }
+    }
+};
+    game.qhly_importSkinInfo(obj);
+});
