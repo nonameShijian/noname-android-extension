58a1a50508c6390c1221e5e7f4aa3a9886c5cb76
shijian <2954700422@qq.com>
2023-06-27 23:08:01 +0800

    在线更新扩展v1.61更新

--- extData.js
+++ extData.js
@@ -1,7 +1,7 @@
 module.exports = {
 	extension: {
 		'在线更新': {
-			version: '1.6',
+			version: '1.61',
 			author: '诗笺',
 			intro: '用于更新无名杀版本，<span style="color: red;">基本实现0崩溃</span>。<br>注: 第一次安装游戏请解压完整包后再用此扩展检查更新',
 		},
--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 window["noname_android_extension"] = {
 	"在线更新": {
-		"version": "1.6",
+		"version": "1.61",
 		"author": "诗笺",
 		"intro": "用于更新无名杀版本，<span style=\"color: red;\">基本实现0崩溃</span>。<br>注: 第一次安装游戏请解压完整包后再用此扩展检查更新",
 		"files": [
