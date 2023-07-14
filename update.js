e3ff6c7ab2c1a082051b6a6661369c71b5a107fe
Tipx-L <138244655+Tipx-L@users.noreply.github.com>
2023-07-15 00:30:33 +0800

    Change from Online Update to Once And For All.

--- update.js
+++ update.js
@@ -1,12 +1,12 @@
 window["noname_android_extension"] = {
-	"在线更新": {
+	"一劳永逸": {
 		"version": "1.61",
 		"author": "诗笺",
 		"intro": "用于更新无名杀版本，<span style=\"color: red;\">基本实现0崩溃</span>。<br>注: 第一次安装游戏请解压完整包后再用此扩展检查更新",
 		"files": [
-			"extension/在线更新/extension.css",
-			"extension/在线更新/extension.js",
-			"extension/在线更新/typings/index.d.ts"
+			"extension/一劳永逸/extension.css",
+			"extension/一劳永逸/extension.js",
+			"extension/一劳永逸/typings/index.d.ts"
 		],
 		"size": "78.01KB"
 	}
