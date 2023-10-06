6892ac6d09371eec74d339a7828f5290ebd5ddd1
Tipx-L <138244655+Tipx-L@users.noreply.github.com>
2023-10-06 08:10:47 -0700

    Decade UI 1.2.0.220114.36

--- update.js
+++ update.js
@@ -1080,8 +1080,8 @@ window["noname_android_extension"] = {
 		"size": "104.43MB"
 	},
 	"十周年UI": {
-		"nonameVersion": "1.10.2.1",
-		"version": "1.2.0.220114.35",
+		"nonameVersion": "1.10.3.1",
+		"version": "1.2.0.220114.36",
 		"author": "Show-K←寰宇星城←disgrace2013←短歌 QQ464598631",
 		"intro": "是最主流的美化扩展，能让无名杀的操作界面更像三国杀，也是许多特效，美化的基础",
 		"files": [
@@ -2022,7 +2022,7 @@ window["noname_android_extension"] = {
 			"extension/十周年UI/image/decoration/name_ye.png",
 			"extension/十周年UI/image/decoration/vs.png"
 		],
-		"size": "162.56MB"
+		"size": "162.54MB"
 	},
 	"千幻聆音": {
 		"version": "4.0.3",
